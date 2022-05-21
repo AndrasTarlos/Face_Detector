33a60eafa939f775b60ab63ce66477ccc8941578
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-05-21 12:03:23 +0200

    Worked on branch Dynamsoft

--- 1_Detector/sockettesting/process.py
+++ 1_Detector/sockettesting/process.py
@@ -1,9 +1,7 @@
 import base64
 import io
 
-import numpy as np
-import ps as ps
-from PIL import Image
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from face_detector import *
@@ -34,15 +32,17 @@ def connect():
     print("User connected")
 
 
-@socketio.on("image")
+@socketio.on("frame")
 def get_face_coordinates(base64_data):
-    im = Image.open(io.BytesIO(base64.b64decode(base64_data)))
-    im.save('image.png', 'PNG')
+    print(base64_data)
+    # im = Image.open(io.BytesIO(base64.b64decode(base64_data)))
+    # im.save('image.png', 'PNG')
 
     '''
     (x, y, w, h) = create_coordinates(img)
     emit("face_coordinates", {"x": x, "y": y, "w": w, "h": h})
     '''
+
 
 if __name__ == "__main__":
     socketio.run(app)
